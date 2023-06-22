16c0d3899f60e125d6be0a9c55aba6eea5d84370
Andrew Willems <39838470+compbiolover@users.noreply.github.com>
2023-06-22 00:09:44 -0400

    Completed NW and NH Permutation Tests

--- Code/markdown/test_3d_knn.R
+++ Code/markdown/test_3d_knn.R
@@ -226,12 +226,11 @@ for (i in unique(mean_neighbor_df_simplified$condition)) {
 
     p_value <- fligner_test$p.value
     p_value_kw <- kw_test$p.value
-    if (test_performed == "fk"){
+    if (test_performed == "fk") {
       p_value_text <- ifelse(p_value < 0.05, paste0("p = ", format(p_value, scientific = TRUE, digits = 3)), "NS")
     } else {
       p_value_text <- ifelse(p_value_kw < 0.05, paste0("p = ", format(p_value_kw, scientific = TRUE, digits = 3)), "NS")
     }
-    
   }
 
 
@@ -274,9 +273,9 @@ p2 <- ggarrange(all_imgs[[4]],
 
 # Saving combo plot
 ggsave(p2,
-       device = plot_type, path = "Outputs/",
-       filename = paste0("mean_intensity_of_cells_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
-       width = 10, height = 10, units = "in", dpi = 600, bg = "white"
+  device = plot_type, path = "Outputs/",
+  filename = paste0("mean_intensity_of_cells_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
+  width = 10, height = 10, units = "in", dpi = 600, bg = "white"
 )
 
 
@@ -382,9 +381,9 @@ combo_plot <- ggarrange(
 
 # Saving combo plot
 ggsave(combo_plot,
-       device = plot_type, path = "Outputs/",
-       filename = paste0("scatter_plot_3d_combo_", "_", time_point, ".", plot_type),
-       width = 14, height = 14, units = "in", dpi = 600, bg = "white"
+  device = plot_type, path = "Outputs/",
+  filename = paste0("scatter_plot_3d_combo_", "_", time_point, ".", plot_type),
+  width = 14, height = 14, units = "in", dpi = 600, bg = "white"
 )
 
 
@@ -495,31 +494,31 @@ for (i in unique(mean_neighbor_df_simplified$condition)) {
   current_cond <- filter(mean_neighbor_df_simplified, condition == i)
   current_cond <- filter(current_cond, mecp2_p == "P")
   current_cond$image <- as.factor(current_cond$image)
-  
+
   # Test for normality using Shapiro-Wilk test
   shapiro_test <- shapiro.test(current_cond$positive_neighborhood_mean)
   if (shapiro_test$p.value >= 0.05) {
     # Data is normally distributed
     print("Data is normally distributed")
-    
+
     # Perform Levene's test for homogeneity of variances
     levene_test <- leveneTest(positive_neighborhood_mean ~ image, data = current_cond)
     print(levene_test)
-    
+
     p_value <- levene_test$"Pr(>F)"[1] # Get the p-value from the test
     p_value_text <- ifelse(p_value < 0.05, paste0("p = ", format(p_value, scientific = FALSE, digits = 3)), "NS")
   } else {
     # Data is not normally distributed
     print("Data is not normally distributed")
-    
+
     # Perform Fligner-Killeen test for homogeneity of variances
     fligner_test <- fligner.test(positive_neighborhood_mean ~ image, data = current_cond)
     print(fligner_test)
-    
-    
+
+
     kw_test <- kruskal.test(positive_neighborhood_mean ~ image, data = current_cond)
     print(kw_test)
-    
+
     p_value <- fligner_test$p.value
     p_value_kw <- kw_test$p.value
     if (test_performed == "fk") {
@@ -528,9 +527,9 @@ for (i in unique(mean_neighbor_df_simplified$condition)) {
       p_value_text <- ifelse(p_value_kw < 0.05, paste0("p = ", format(p_value_kw, scientific = TRUE, digits = 3)), "NS")
     }
   }
-  
-  
-  
+
+
+
   p1 <- ggplot(aes(x = image, y = positive_neighborhood_mean), data = current_cond) +
     geom_violin(draw_quantiles = c(0.25, 0.50, 0.75)) +
     geom_sina(aes(colour = image)) +
@@ -557,21 +556,21 @@ for (i in unique(mean_neighbor_df_simplified$condition)) {
 
 
 p2 <- ggarrange(all_imgs[[2]],
-                all_imgs[[1]],
-                all_imgs[[4]],
-                all_imgs[[3]],
-                ncol = 2,
-                nrow = 2,
-                align = "hv",
-                labels = "AUTO", font.label = list(size = 18, face = "bold")
+  all_imgs[[1]],
+  all_imgs[[4]],
+  all_imgs[[3]],
+  ncol = 2,
+  nrow = 2,
+  align = "hv",
+  labels = "AUTO", font.label = list(size = 18, face = "bold")
 )
 
 
 # Saving combo plot
 ggsave(p2,
-       device = plot_type, path = "Outputs/",
-       filename = paste0("positive_neighborhood_mean_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
-       width = 10, height = 10, units = "in", dpi = 600, bg = "white"
+  device = plot_type, path = "Outputs/",
+  filename = paste0("positive_neighborhood_mean_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
+  width = 10, height = 10, units = "in", dpi = 600, bg = "white"
 )
 
 
@@ -599,43 +598,42 @@ mean_neighbor_df_simplified <- filter(mean_neighbor_df_simplified, mecp2_p == "P
 for (i in unique(mean_neighbor_df_simplified$condition)) {
   current_cond <- filter(mean_neighbor_df_simplified, condition == i)
   current_cond$image <- as.factor(current_cond$image)
-  
+
   # Test for normality using Shapiro-Wilk test
   shapiro_test <- shapiro.test(current_cond$mean)
   if (shapiro_test$p.value >= 0.05) {
     # Data is normally distributed
     print("Data is normally distributed")
-    
+
     # Perform Levene's test for homogeneity of variances
     levene_test <- leveneTest(mean ~ image, data = current_cond)
     print(levene_test)
-    
+
     p_value <- levene_test$"Pr(>F)"[1] # Get the p-value from the test
     p_value_text <- ifelse(p_value < 0.05, paste0("p = ", format(p_value, scientific = FALSE, digits = 3)), "NS")
   } else {
     # Data is not normally distributed
     print("Data is not normally distributed")
-    
+
     # Perform Fligner-Killeen test for homogeneity of variances
     fligner_test <- fligner.test(mean ~ image, data = current_cond)
     print(fligner_test)
-    
-    
+
+
     kw_test <- kruskal.test(mean ~ image, data = current_cond)
     print(kw_test)
-    
+
     p_value <- fligner_test$p.value
     p_value_kw <- kw_test$p.value
-    if (test_performed == "fk"){
+    if (test_performed == "fk") {
       p_value_text <- ifelse(p_value < 0.05, paste0("p = ", format(p_value, scientific = TRUE, digits = 3)), "NS")
     } else {
       p_value_text <- ifelse(p_value_kw < 0.05, paste0("p = ", format(p_value_kw, scientific = TRUE, digits = 3)), "NS")
     }
-    
   }
-  
-  
-  
+
+
+
   p1 <- ggplot(aes(x = image, y = mean), data = current_cond) +
     geom_violin(draw_quantiles = c(0.25, 0.50, 0.75)) +
     geom_sina(aes(colour = image)) +
@@ -662,21 +660,21 @@ for (i in unique(mean_neighbor_df_simplified$condition)) {
 
 
 p2 <- ggarrange(all_imgs[[2]],
-                all_imgs[[1]],
-                all_imgs[[4]],
-                all_imgs[[3]],
-                ncol = 2,
-                nrow = 2,
-                align = "hv",
-                labels = "AUTO", font.label = list(size = 18, face = "bold")
+  all_imgs[[1]],
+  all_imgs[[4]],
+  all_imgs[[3]],
+  ncol = 2,
+  nrow = 2,
+  align = "hv",
+  labels = "AUTO", font.label = list(size = 18, face = "bold")
 )
 
 
 # Saving combo plot
 ggsave(p2,
-       device = plot_type, path = "Outputs/",
-       filename = paste0("mean_intensity_of_cells_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
-       width = 10, height = 10, units = "in", dpi = 600, bg = "white"
+  device = plot_type, path = "Outputs/",
+  filename = paste0("mean_intensity_of_cells_violin_plot_combo_plot_3d_", test_performed, "_", time_point, ".", plot_type),
+  width = 10, height = 10, units = "in", dpi = 600, bg = "white"
 )
 
 
@@ -733,16 +731,16 @@ for (c in unique(mean_neighbor_df_simplified$condition)) {
     ellipse = FALSE,
     mean.point = FALSE
   )
-  
+
   # p2 for the single annotation context
   p2 <- ggpar(p1,
-              title = paste0("K = ", k_n, " | ", c, " | 3D | ", toupper(time_point)),
-              font.main = c(18, "bold"),
-              font.x = c(18, "bold"),
-              font.y = c(18, "bold"),
-              ggtheme = theme(plot.title = element_text(hjust = 0.5))
+    title = paste0("K = ", k_n, " | ", c, " | 3D | ", toupper(time_point)),
+    font.main = c(18, "bold"),
+    font.x = c(18, "bold"),
+    font.y = c(18, "bold"),
+    ggtheme = theme(plot.title = element_text(hjust = 0.5))
   )
-  
+
   p2 <- p2 +
     theme(
       axis.line = element_line(linewidth = 1.25, lineend = "round"),
@@ -755,7 +753,7 @@ for (c in unique(mean_neighbor_df_simplified$condition)) {
       legend.position = "bottom"
     ) +
     labs(color = "Image", fill = "Image")
-  
+
   # Change to p2 for single annotation case, p5 otherwise
   plots[[paste0(c)]] <- p2
   counter <- counter + 1
@@ -782,9 +780,9 @@ combo_plot <- ggarrange(
 
 # Saving combo plot
 ggsave(combo_plot,
-       device = plot_type, path = "Outputs/",
-       filename = paste0("scatter_plot_3d_combo_", "_", time_point, ".", plot_type),
-       width = 14, height = 14, units = "in", dpi = 600, bg = "white"
+  device = plot_type, path = "Outputs/",
+  filename = paste0("scatter_plot_3d_combo_", "_", time_point, ".", plot_type),
+  width = 14, height = 14, units = "in", dpi = 600, bg = "white"
 )
 
 
@@ -909,14 +907,14 @@ all_12wk_dfs <- list.files(path = "Outputs/", pattern = "*_12wk.csv")
 # 6 week rhos
 all_rhos <- list()
 counter <- 1
-for (f in all_6wk_dfs){
-  current_k <- read.csv(paste0("Outputs/",f))
-  for ( c in unique(current_k$condition)){
+for (f in all_6wk_dfs) {
+  current_k <- read.csv(paste0("Outputs/", f))
+  for (c in unique(current_k$condition)) {
     current_k$rho <- rep(0, nrow(current_k))
     current_cond <- filter(current_k, condition == c)
     current_cond <- filter(current_cond, mecp2_p == "P")
     current_rho <- cor.test(x = current_cond$mean, y = current_cond$positive_neighborhood_mean, method = "spearman")
-    all_rhos[[paste0(c,"_",f)]] <- current_rho$estimate
+    all_rhos[[paste0(c, "_", f)]] <- current_rho$estimate
     counter <- counter + 1
   }
 }
@@ -932,14 +930,14 @@ six_wk_rho_df <- six_wk_rho_df %>%
 # 12 week rhos
 all_rhos <- list()
 counter <- 1
-for (f in all_12wk_dfs){
-  current_k <- read.csv(paste0("Outputs/",f))
-  for ( c in unique(current_k$condition)){
+for (f in all_12wk_dfs) {
+  current_k <- read.csv(paste0("Outputs/", f))
+  for (c in unique(current_k$condition)) {
     current_k$rho <- rep(0, nrow(current_k))
     current_cond <- filter(current_k, condition == c)
     current_cond <- filter(current_cond, mecp2_p == "P")
     current_rho <- cor.test(x = current_cond$mean, y = current_cond$positive_neighborhood_mean, method = "spearman")
-    all_rhos[[paste0(c,"_",f)]] <- current_rho$estimate
+    all_rhos[[paste0(c, "_", f)]] <- current_rho$estimate
     counter <- counter + 1
   }
 }
@@ -987,8 +985,166 @@ ggsave(plot = p1, filename = "rhos_across_k_1_to_15_all_conds.svg", path = "Outp
 ggsave(plot = p1, filename = "rhos_across_k_1_to_15_all_conds.png", path = "Outputs/", device = "png", height = 9, width = 16, units = "in", dpi = 600)
 
 
+# Permutation test for 3D 6 wk data
+all_data <- list()
+all_p_scores <- list()
+counter <- 1
+apply_intensity <- TRUE
+p_only <- TRUE
+calc_mean_intensity <- TRUE
+neighbor_means <- c()
+random_num_neighbors <- TRUE
+k_neighbors <- 5
+all_rhos_stored <- c()
+all_p_values_stored <- c()
 
-# Permutation test
+
+# 6 week data
+threed_data <- read.csv("Data/Old data/Jacob/3D_MECP2_6WO_combined_dataset.csv")
+threed_data <- threed_data %>%
+  select(Filename, Mean, CX..pix., CY..pix., CZ..pix., MECP2) %>%
+  rename(mecp2_p = MECP2) %>%
+  rename_all(tolower) %>%
+  rename(
+    x = cx..pix.,
+    y = cy..pix.,
+    z = cz..pix.,
+  ) %>%
+  mutate(hemisphere = str_extract(filename, "(LH|RH)")) %>%
+  mutate(condition = str_extract(filename, "(NW|NH|SW|SH)")) %>%
+  filter(mecp2_p == "P") %>%
+  mutate(id = 1:nrow(.)) %>%
+  # Change this line to filter to different condition
+  filter(condition == "NW")
+
+threed_data <- threed_data %>% group_by(filename)
+all_imgs <- threed_data %>% group_split(threed_data)
+
+
+for (n in 1:1000) {
+  for (i in 1:length(all_imgs)) {
+    df <- all_imgs[[i]]
+    df$knn_5_label <- rep(0, nrow(df))
+    for (r in 1:nrow(df)) {
+      observation <- df[r, ]
+      other_points <- df[-r, ]
+
+      distances <- euclidean_distance_3d(
+        observation$x, observation$y, observation$z,
+        other_points$x, other_points$y, other_points$z
+      )
+
+      # Selecting random neighbors
+      if (random_num_neighbors) {
+        k_nearest_indices <- sample(nrow(other_points), k_neighbors)
+        k_nearest_points <- other_points[k_nearest_indices, ]
+      } else {
+        distances <- euclidean_distance_3d(
+          observation$x, observation$y, observation$z,
+          other_points$x, other_points$y, other_points$z
+        )
+        k_nearest_indices <- order(distances)[1:k_neighbors]
+        k_nearest_points <- other_points[k_nearest_indices, ]
+      }
+
+      intensity_info <- other_points[k_nearest_indices, ]
+      add_p_score <- sum((intensity_info$mecp2_p == "P")) / k_neighbors
+      observation$knn_5_label <- add_p_score
+      all_p_scores[[counter]] <- add_p_score
+
+      # Calculate distances with intensity information
+      if (apply_intensity) {
+        distances <- distances[k_nearest_indices]
+        distances_updated <- rescale(distances * intensity_info$mean, to = c(0, 1))
+        distances_updated <- sort(distances_updated, decreasing = FALSE)
+
+        # Calculate updated distances with intensity information for just P neighbors
+        if (p_only) {
+          intensity_info <- filter(intensity_info, mecp2_p == "P")
+          if (nrow(intensity_info) == 0) {
+            writeLines("This sample has only N neighbors.\nIt is being excluded from this analysis.")
+            next
+          }
+        }
+        if (calc_mean_intensity) {
+          df$positive_neighborhood_mean <- rep(0, nrow(df))
+          df$rho <- rep(0, nrow(df))
+          mean_of_neighbors <- mean(intensity_info$mean)
+          observation$positive_neighborhood_mean <- mean_of_neighbors
+          all_data[[counter]] <- observation
+          counter <- counter + 1
+        }
+      }
+    }
+  }
+  # Binding all observations from this group of images 
+  test_df <- bind_rows(all_data)
+  
+  # Calculating rho
+  cor_res <- cor.test(x = test_df$mean, y = test_df$positive_neighborhood_mean, method = "spearman")
+  current_p <- cor_res$p.value
+  current_rho <- as.numeric(as.vector(unlist(cor_res$estimate)))
+  all_rhos_stored <- c(all_rhos_stored, current_rho)
+  all_p_values_stored <- c(all_p_values_stored, current_p)
+}
+
+nh_data <- data.frame(condition = rep("NH", times = length(all_rhos_stored)), rho = all_rhos_stored, p_value = all_p_values_stored)
+nw_data <- data.frame(condition = rep("NW", times = length(all_rhos_stored)), rho = all_rhos_stored, p_value = all_p_values_stored)
+# write.csv(nh_data, "Outputs/nh_1000_permutation_6wk_df.csv")
+# write.csv(nw_data, "Outputs/nw_1000_permutation_6wk_df.csv")
+
+#NH histogram
+p1 <- ggplot(data = nh_data, aes(x = rho)) +
+  geom_histogram(color = "steelblue", fill = "steelblue") +
+  theme(
+    panel.background = element_blank(),
+    axis.title = element_text(size = 18, face = "bold"),
+    axis.text = element_text(size = 16),
+    axis.ticks = element_line(linewidth = 1.25),
+    axis.ticks.length = unit(0.2, units = "cm"),
+    strip.text = element_text(size = 18, face = "bold"),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    axis.line = element_line(colour = "black", linewidth = 1.25)
+  ) +
+  ggtitle(paste0("K = 5 | ", unique(nh_data$condition), " | ", length(all_rhos_stored))) +
+  xlab(expression(rho)) +
+  ylab("Count") +
+  scale_y_continuous(expand = c(0, 0))+
+  geom_vline(xintercept = 0.44, color = "red")
+  # facet_wrap(~condition, nrow = 2, ncol = 2)
+
+# SVG
+ggsave(plot = p1, filename = "nh_random_knn_5_vs_rho_histogram_6wk.svg", device = "svg", width = 10, height = 10, units = "in", path = "Outputs/knn_analysis/plots/")
+
+# PNG
+ggsave(plot = p1, filename = "nh_random_knn_5_vs_rho_histogram_6wk.png", device = "png", width = 10, height = 10, units = "in", path = "Outputs/knn_analysis/plots/")
+
+
+
+#NW histogram
+p1 <- ggplot(data = nw_data, aes(x = rho)) +
+  geom_histogram(color = "steelblue", fill = "steelblue") +
+  theme(
+    panel.background = element_blank(),
+    axis.title = element_text(size = 18, face = "bold"),
+    axis.text = element_text(size = 16),
+    axis.ticks = element_line(linewidth = 1.25),
+    axis.ticks.length = unit(0.2, units = "cm"),
+    strip.text = element_text(size = 18, face = "bold"),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    axis.line = element_line(colour = "black", linewidth = 1.25)
+  ) +
+  ggtitle(paste0("K = 5 | ", unique(nw_data$condition), " | ", length(all_rhos_stored))) +
+  xlab(expression(rho)) +
+  ylab("Count") +
+  scale_y_continuous(expand = c(0, 0))+
+  geom_vline(xintercept = 0.27, color = "red")
+
+# SVG
+ggsave(plot = p1, filename = "nw_random_knn_5_vs_rho_histogram_6wk.svg", device = "svg", width = 10, height = 10, units = "in", path = "Outputs/knn_analysis/plots/")
+
+# PNG
+ggsave(plot = p1, filename = "nw_random_knn_5_vs_rho_histogram_6wk.png", device = "png", width = 10, height = 10, units = "in", path = "Outputs/knn_analysis/plots/")
 
 
 
